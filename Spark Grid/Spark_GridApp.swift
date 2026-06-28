7f93e96c48319962821dbb55fb59615bbb4e6629
Sam Parker <parkersam1123@gmail.com>
2026-06-28 16:27:58 -0400

    Initial Commit

---
+++ Spark Grid/Spark_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spark_GridApp.swift
+//  Spark Grid
+//
+//  Created by Sam Parker on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Spark_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
